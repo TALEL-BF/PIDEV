b343f8ad47129751b9dea79a12447bb2915bc9a5
manelhosni <164367229+manelhosni@users.noreply.github.com>
2026-02-08 14:39:46 +0100

    connextion avec la base

---
+++ src/main/java/Entites/RDV.java
@@ -0,0 +1,81 @@
+/*package Entites;
+
+import java.sql.Date;
+
+public class RDV {
+    private int id;
+    private String nom;
+    private String prenom;
+    private int age;
+    private int date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public RDV() {
+    }
+
+    public RDV(int id, String nom, String prenom, int age, int date) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+        this.date = date;
+    }
+
+    public RDV(String nom, String prenom, int age, int date) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "RDV{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", age=" + age +
+                ", date=" + date +
+                '}';
+    }
+}
+*/
---
+++ src/main/java/test/Main.java
@@ -0,0 +1,20 @@
+package test;
+
+//import Entites.RDV;
+//import Services.RDVServices;
+import Utils.Mydatabase;
+
+public class Main {
+    public static void main(String[] args) {
+        Mydatabase.getInstance();
+
+        //RDVServices rdvs = new RDVServices();
+
+        //RDV rdv = new RDV(4,"Saleh" , "YASalih" , 25 , 2026);
+
+        //rdvs.ajoutRDV(rdv);
+        //rdvs.supprimerRDV(3);
+        //rdvs.modifierRDV(rdv);
+       // System.out.println(rdvs.afficherRDV());
+    }
+}
